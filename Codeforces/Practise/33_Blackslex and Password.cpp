30b2a243b5c83a28d094158709f281bf4d5cfc61
SKEL1NJA <khannauday55@gmail.com>
2026-04-18 08:56:26 +0530

    Codeforces problem 'Blackslex and Password' submitted and accepted

---
+++ Codeforces/Practise/33_Blackslex and Password.cpp
@@ -0,0 +1,34 @@
+// Problem Link: https://codeforces.com/contest/2179/problem/A
+// Submission Link: https://codeforces.com/contest/2179/submission/371588458
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define fi first
+#define se second
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve() {
+    int k, x;
+    cin >> k >> x;
+
+    cout << k * x + 1 << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+    
+    return 0;
+}
