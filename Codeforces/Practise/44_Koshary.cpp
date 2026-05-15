e1229111a744e28f90d0a0dee3a95ce1a0a1d679
SKEL1NJA <formysocials69420@gmail.com>
2026-05-15 10:17:03 +0530

    Codeforces problem 'Koshary' submitted and accepted

---
+++ Codeforces/Practise/44_Koshary.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define fi first
+#define se second
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve() {
+    int x,y;
+    cin>>x>>y;
+
+    if(x%2==0 && y%2==0){
+        cout<<"YES"<<endl;
+        return;
+    }
+
+    if(x%2!=0 && y%2!=0){
+        cout<<"NO"<<endl;
+        return;
+    }
+
+    if((x%2==0 && y%2!=0) || (x%2!=0 && y%2==0)){
+        cout<<"YES"<<endl;
+        return;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+    
+    return 0;
+}
