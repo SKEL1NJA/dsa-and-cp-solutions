196b5b59f5b6156e95a9ab4d72765d4488a2f0d5
SKEL1NJA <formysocials69420@gmail.com>
2026-05-15 10:29:46 +0530

    Codeforces problem 'Party Monster' submitted and accepted

---
+++ Codeforces/Practise/45_Party Monster.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vll vector<ll>
+#define pll pair<ll,ll>
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define fi first
+#define se second
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve() {
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+
+    int cnt1=0, cnt2=0;
+
+    for(int i=0;i<n;i++){
+        if(s[i]=='(') cnt1++;
+        else cnt2++;
+    }
+
+    if(cnt1==cnt2) cout<<"YES"<<endl;
+    else cout<<"NO"<<endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+    
+    return 0;
+}
