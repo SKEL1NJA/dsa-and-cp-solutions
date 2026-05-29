d94afac41667e52ef865b097a083bf841c7f4e6c
SKEL1NJA <formysocials69420@gmail.com>
2026-05-29 22:34:07 +0530

    Codeforces problem 'A Simple Sequence' submitted and accepted

---
+++ Codeforces/Practise/47_A Simple Sequence.cpp
@@ -0,0 +1,40 @@
+// Problem Link: https://codeforces.com/contest/2210/problem/A
+// Submission Link: https://codeforces.com/contest/2210/submission/376543030
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define vll vector<ll>
+#define pll pair<ll, ll>
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define fi first
+#define se second
+
+const ll MOD = 1e9 + 7;
+const ll INF = 1e18;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    while (n >= 1)
+    {
+        cout << n << " ";
+        n--;
+    }
+    cout << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) solve();
+
+    return 0;
+}
